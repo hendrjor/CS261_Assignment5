00b3c3ed1b26429d2d58b7451574573004268ba4
hendrjor <hendrjor@oregonstate.edu>
2021-03-09 21:12:09 -0800

    Ver - 3.6

--- min_heap.py
+++ min_heap.py
@@ -137,7 +137,7 @@ class MinHeap:
             x = da.get_at_index(i)
             self.heap.append(x)
 
-        start_index = int((length - 2) / 2)
+        start_index = int((length / 2) - 1)
         for index in range(start_index, -1, -1):
             self.build_heap_helper(index, length)
 
@@ -145,7 +145,7 @@ class MinHeap:
 
         # self.build_heap_helper(parent_index, length, switch)
 
-    # def build_heap_helper(self, parent_index, length, switch):
+    # def build_heap_helper(self, parent_index, length):
     #     """"""
     #     if parent_index == -1:
     #         return
@@ -171,6 +171,10 @@ class MinHeap:
     #             min_child = child_right
     #             min_child_index = child_right_index
     #
+    #     # print(parent)
+    #     # print(min_child)
+    #     # print("\n")
+    #
     #     if parent > min_child:
     #         temp_parent_index = parent_index  # temp variable to hold the parent's index during the swap
     #         parent_index = min_child_index
@@ -178,15 +182,13 @@ class MinHeap:
     #         self.heap.set_at_index(parent_index, parent)  # swaps the parent and child values
     #         self.heap.set_at_index(min_child_index, min_child)
     #
-    #         if switch is True:
-    #             self.build_heap_helper(parent_index, length, True)
-    #             return
-    #         self.build_heap_helper(temp_parent_index - 1, length, False)
+    #         self.build_heap_helper(parent_index, length)
+    #         self.build_heap_helper(temp_parent_index - 1, length)
     #         return
-    #
-    #     else:
-    #         parent_index -= 1
-    #         self.build_heap_helper(parent_index, length, False)
+
+        # else:
+        #     parent_index -= 1
+        #     self.build_heap_helper(parent_index, length)
 
     # def build_heap_helper(self, index, length):
     #     """Recursively builds a heap from an unorganized list"""
@@ -195,6 +197,7 @@ class MinHeap:
     #         leaf = True
     #
     #     parent = self.heap.get_at_index(index)
+    #     min_index = index
     #     child_left_index = int((2 * index))
     #     child_right_index = int((2 * index) + 1)
 
@@ -222,6 +225,37 @@ class MinHeap:
         #         self.heap.swap(index, min_child_index)
         #         self.build_heap_helper(min_child_index, length)
 
+        # if child_left_index < length and self.heap.get_at_index(child_left_index) < self.heap.get_at_index(min_index):
+        #     min_index = child_left_index
+        #
+        # if child_right_index < length and self.heap.get_at_index(child_right_index) < self.heap.get_at_index(min_index):
+        #     min_index = child_right_index
+
+
+        # if not leaf:
+        #     left_child = self.heap.get_at_index(child_left_index)
+        #     right_child = self.heap.get_at_index(child_right_index)
+        #     if child_left_index < length and left_child < parent:
+        #         min_index = child_left_index
+        #
+        #     if child_right_index < length and right_child < self.heap.get_at_index(min_index):
+        #         min_index = child_right_index
+        #
+        #     if min_index != index:
+        #         self.heap.swap(index, min_index)
+        #         self.build_heap_helper(min_index, length)
+
+            # if parent > left_child or parent > right_child:
+
+                # if left_child < right_child:
+                #     self.heap.swap(index, child_left_index)
+                #     self.build_heap_helper(child_left_index, length)
+                #
+                # else:
+                #     self.heap.swap(index, child_right_index)
+                #     self.build_heap_helper(child_right_index, length)
+
+
         # if not leaf:
         #     left_child = self.heap.get_at_index(child_left_index)
         #     right_child = self.heap.get_at_index(child_right_index)
@@ -235,44 +269,30 @@ class MinHeap:
         #             self.heap.swap(index, child_right_index)
         #             self.build_heap_helper(child_right_index, length)
 
-    def build_heap_helper(self, length, index):
+    def build_heap_helper(self, index, length):
         """"""
-        largest = index
-        # parent = self.heap.get_at_index(index)
-        child_left_index = int((2 * index) + 1)
-        child_right_index = int((2 * index) + 2)
+        min_index = index
+        child_left_index = (2 * index) + 1
+        child_right_index = (2 * index) + 2
 
         # left_child = self.heap.get_at_index(child_left_index)
         # right_child = self.heap.get_at_index(child_right_index)
 
-        if child_left_index < length and self.heap.get_at_index(child_left_index) > self.heap.get_at_index(largest):
-            largest = child_left_index
+        # print(min_index)
+        # print(child_left_index)
+        # print(child_right_index)
+        # print(length)
+        # print("\n")
 
-        if child_right_index < length and self.heap.get_at_index(child_right_index) > self.heap.get_at_index(largest):
-            largest = child_right_index
+        if child_left_index < length and self.heap.get_at_index(child_left_index) < self.heap.get_at_index(min_index):
+            min_index = child_left_index
 
-        if largest != index:
-            self.heap.swap(index, largest)
-            self.build_heap_helper(length, index)
+        if child_right_index < length and self.heap.get_at_index(child_right_index) < self.heap.get_at_index(min_index):
+            min_index = child_right_index
 
-        # if child_right_index >= length and child_left_index >= length:
-        #     return
-        #
-        # elif child_right_index >= length:
-        #     child_left = self.heap.get_at_index(child_left_index)
-        #     min_child = child_left
-        #     min_child_index = child_left_index
-        # else:
-        #     child_left = self.heap.get_at_index(child_left_index)
-        #     child_right = self.heap.get_at_index(child_right_index)
-        #
-        #     if child_left <= child_right:
-        #         min_child = child_left
-        #         min_child_index = child_left_index
-        #     else:
-        #         min_child = child_right
-        #         min_child_index = child_right_index
-
+        if min_index != index:
+            self.heap.swap(index, min_index)
+            self.build_heap_helper(min_index, length)
 
 # BASIC TESTING
 if __name__ == '__main__':
@@ -318,10 +338,10 @@ if __name__ == '__main__':
 
     print("\n")
 
-    da = DynamicArray([-784, -83, -860, -628, 596, 821, -453, 375, 402, -298, -44, 851, 271, 944, 295, 954, 533, -965, 883, 721, -46, 47, -625, -317, 890, 533, -765, -276, 958, 899, 148, 358, 365, 339, -554, -111, -390, 94, -134, -217, 252, 547, -757, -809, 660, -607, 188, -119, -175, 581, -134, 659, -237, 993, 39, 848, 904, -729, 273, 151, -558, -232, 308, 579, -523, 618, -279, 988, 854, 934, -146, 124, -543, 874, -419, 920, -702, 33, 705, 963, 951, 743, 364, 32, -979, 842, -119, 819, 430, -854, 531, -486, -68, 184, 526, -328, 842, -889, -196, 465, -224, 367, -512, 631, -217, 956, -174, 982, -332, -209, -786, -997, -225, 222, 330, -202, -535, 702, 490, 759, 787, 269, 172, 100, 386, 652, 196, 342, 635, 629, 959, 611, -10, -602, 592, -628, 910, 504, -945, 545, -123, -25, -251, -532, -774, 204, 525, 231, 363, -865, 965, -410, 216, -629, 570, -89, 286, -824, 212, 400, -5, -181, 135, -683, -778, -396, -80, 770, -140, 68, 512, 415, 340, -189, 516, 598, 753, 910, -83, -344, 839, 834, 30, -695, -200, -952, 898, 146, 750, -790, -5, -804, -748, 480, -24, -108, -404, 835, -42, -108, 431, -853, -309, -92, -512, 90, 264, 218, 826, -307, 640, 568, -646, 633, 689, 178, -217, 34, 579, -262, -637, 490, 321, 63, 978, 78, -314, -348, 525, -151, -88, -766, 303, 578, -834, -779, -257, -243, 161, -761, 512, -903, 554, -781, 970, 942, 850, -393, -183, -350, -755, 787, -851, -79, 756, -818, -494, 116, 469, 254, 470, -789, -516, 880, -245, 276, -438, 99, -187, -683, 224, -537, 876, -661, 60, -744, 321, 575, 163, -629, 290, 281, 69, -833, -481, -313, -524, 863, -119, 978, -755, -34, -897, -845, 135, 544, -66, -863, 474, 674, 15, -902, -72, 966, 965, -109, 227, -127, -151, -71, 122, 765, 368, 330, -164, -859, -11, 328, -630, -605, 239, 838, 683, 826, 101, 939, 800, 205, 142, -663, -587, -898, -79, 799, 780, 704, 927, -533, 121, 168, -927, 232, -332, -387, 289, 46, -316, -701, -394, -426, 178, 730, 127, 855, 766, -937, 191, -251, -670, -71, -239, -472, 211, 550, 469, 39, -712, 16, 233, -456, -777, -300, 325, -522, -360, -220, -343, -75, -859, -821, -381, 704, -709, -253, 932, -686, 417, 0, -780, 377, -998, -357, -893, 907, 155, -77, 116, 902, 848, 512, 566, -365, 53, -775, -916, -437, -828, -325, 979, 434, 478, 738, 291, 310, 459, -723, 192, -757, 420, -874, 554, -77, -969, -79, 248, 198, -120, 253, -398, -167, 68, -742, 13, 676, 290, 494, -322, 148, 633, 257, 632, 129, -952, 388, -241, -697, 298, 247, -437, -382, 580, -190, -751, -232, 103, 405, -804, 500, 468, -281, -384, 517, 296, -677, 273, -823, -766, -82, -183, 898, -194, -516, 475, -843, -792, 522, 35, 118, -732, 890, -407, -300, -129, 242, 489, -490, -771, -62, 631, 847, 5, 287, 418, -640, 476, -623, 295, -253, -111, 680, -575, 293, 99, 637, 172, 609, -191, -135, 177, -121, 667, 142, 502, 616, -233, 309, -535, 170, 357, -64, 65, -365, 396, 931, 730, -414, 932, 496, 830, 991, 527, -237, -904, -279, -625, -644, -382, -930, 56, 455, -405, 119, -138, -937, -795, -685, 138, -285, -812, -288, 404, -567, 43, 104, 842, -260, -428, 411, 865, -531, -18, 44, 686, 734, 962, 947, 868, 329, 358, -615, -501, 90, 394, 172, -466, 302, 944, -351, 962, -324, -517, 123, -685, 973, 218, -108, 683, 968, 346, -245, 290, 897, -349, -112, 920, 350, 964, 55, -101, -459, -9, -377, -308, 603, -109, -526, -958, 347, 473, 317, -964, -510, -125, -475, -785, 668, -573, 583, -433, -751, 500, -524, -270, 30, 917, -495, -5, -308, -518, 578, -985, -811, -767, 663, 857, 300, 85, -122, 668, -81, -724, -718, -226, 95, -359, 917, -83, -664, -236, -916, -148, 276, 122, -295, -816, -732, -857, -185, 244, -376, 488, 310, 961, -277, 217, 114, -364, -466, 2, 396, -485, 792, -550, 822, -644, -430, 331, 789, -805, 786, -753, -204, -759, 733, -819, -205, -859, -162, -372, 624, -164, 287, 55, 818, -793, -897, 230, -599, -814, 72, 414, -697, 951, 329, 447, 947, -154, -644, -445, -281, -722, -125, 152, -917, 203, 871, 939, -593, -849, -206, -400, 1, -226, -482, -396, 424, 876, -579, -612, 432, 831, -730, 837, 575, -587, 268, -656, -267, -599, -523, 713, 616, -792, -881, 734, 237, 789, -405, 699, -830, -451, -88, 43, 446, -98, -270, 147, -968, 672, -9, -412, 68, -337, -988, 623, -324, 930, 822, 163, 576, -23, 164, 490, 255, -612, 225, -75, 7, 447, -389, 913, 803, -308, -691, -23, 430, 383, 791, 804, 15, -201, -978, 298, -496, 298, -9, -264, -784, 58, 598, -91, 328, -761, -425, -35, -787, 515, -834, 467, -550, -446, -615, 235, -23, -974, 195, 350, -176, 960, -973, 614, 48, 937, -405, -151, -107, 210, 300, -516, 873, -338, -527, -518, -425, -464, -372, 607, -258, 822, 41, 669, 703, -378, 139, -250, 931])
+    da = DynamicArray([-809, -189, 122, 959, -401, -609, 169, 205, -463, -496, 886, -67, -334, 827, 704, 251, -904, 120, 915, 355, -296, -758, 676, 405, -461, 707, -491, -66, -376, 467, 120, 761, -563, 434, 876, 760, 222, 722, 828, -660, -108, 796, -126, 147, -358, -840, 650, -136, -182, -831, -533, -406, -791, 435, -72, 266, -939, 395, 963, -890, 706, -469, 583, -493, 707, -394, 416])
     # da = DynamicArray([500, 2, 11, 8, 6, 20, 1, 3, 7])
     h = MinHeap([1, 1, 1, 1, 11, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
     print(h)
     h.build_heap(da)
     print(h)
-    # print(da)
+    print(da)
